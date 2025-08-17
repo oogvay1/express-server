0d984a1419488c3fa5caff8c53d6c4a3c721469c
Oybek Coder <bekmirzayevoybek575@gmail.com>
2025-08-17 12:46:42 +0500

    Revert "Add express"

--- .history/index_20250811173620.js
+++
@@ -1,7 +0,0 @@
-const express = require( "express" )
-
-const app = express()
-
-app.get( "/health", ( req, res ) => res.send( { message: "OKAY!!!" } ) )
-
-app.listen( 3000, () => console.info( "Server running at :3000" ) )
